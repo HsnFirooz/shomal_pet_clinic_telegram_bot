b27679ab5d2d25f2fc931167e55af1bc85471c2b
hsnfirooz <hsnfirooz@yahoo.com>
2020-04-29 13:18:21 +0430

    add multiple guardian feature & user can register as a pet guardian

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import config
 
+import redis
+import datetime
+import json
+
 from telegram import (InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler, CallbackQueryHandler)
@@ -19,20 +23,20 @@ ADMIN_MAIN_MANU = 100
 ADD_CASE = 101
 DEL_CASE = 102
 UPDATE_CASE = 103
-GET_CASE_ID = 104
+SET_CASE_ID = 104
 ALL_CASE_INFO = 105
 GET_VISITED_DATE = 106
 ADMIN_TIMER = 107
 SET_PET_NAME = 108
 
-#TODO: Redis server
-patients = {}
+redis_db = redis.Redis(host='127.0.0.1', port='6379', db=0)
 
 #TODO Better keyboard -> conversation Handler
-reply_keyboard = [['new case', 'update case'],
-                  ['del case', 'all case info'],
-                  ['broadcast', 'narrowcast']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+user_reply_keyboard = [['new case', 'update case'],
+                        ['del case', 'all case info'],
+                        ['broadcast', 'narrowcast']]
+admin_markup = ReplyKeyboardMarkup(user_reply_keyboard, one_time_keyboard=True)
 
 def admin_start(update, context):
     usr = update.effective_user
@@ -40,18 +44,23 @@ def admin_start(update, context):
     if is_admin(username):
         update.message.reply_text(f'Welcome Back {username}')
         logger.info('%s logged in as the admin user', username)
-        admin_main_menu(update)
-        return ADMIN_MAIN_MANU
+        return admin_main_menu(update)
+    
     else:
         update.message.reply_text('Hmmm... How did you find about this command? ADMINS ONLY')
-        logger.warning('%s tried to login in as administrator')
+        logger.warning('%s tried to login in as administrator', username)
 
 def admin_main_menu(update):
-    update.message.reply_text(f'What do you want to do?', reply_markup=markup)
+    admin_reply_keyboard = [['new case', 'update case'],
+                            ['del case', 'all case info'],
+                            ['broadcast', 'narrowcast']]
+    admin_markup = ReplyKeyboardMarkup(admin_reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text(f'What do you want to do?', reply_markup=admin_markup)
+    return ADMIN_MAIN_MANU
 
 def admin_add_case(update, context):
     update.message.reply_text("Please Enter the case ID")
-    return GET_CASE_ID 
+    return SET_CASE_ID 
 
 def admin_del_case(update, context):
     return
@@ -61,42 +70,47 @@ def admin_update_case(update, context):
 
 def admin_all_case_info(update, context):
     print(patients)
-    admin_main_menu(update)
-    return ADMIN_MAIN_MANU
+    return admin_main_menu(update)
 
 def is_admin(user):
     return user in ADMIN_USERNAMES
 
 def get_case_id(update, context):
     case_id = update.message.text
-
+    context.user_data['case_id'] = case_id
     if case_exsits(case_id):
+        
         update.message.reply_text(f'a case with this id: {case_id} already exsists!')
         update.message.reply_text(f'do you want to submit a new visit?')
-        return UPDATE_CASE
+        return GET_VISITED_DATE #TODO: Nested Coversation: Yes, No
     else:
         update.message.reply_text(f'a new case with id #{case_id} has been created')
-        context.user_data['case_id'] = case_id
         update.message.reply_text('enter the Pet name')
         return SET_PET_NAME
 
 def set_pet_name(update, context):
     pet_name = update.message.text
     context.user_data['pet_name'] = pet_name
-    update.message.reply_text(f'Nice Name, I\'ll remember that, enter {pet_name}\'s visit')
+    update.message.reply_text(f'Nice Name, I\'ll remember that, enter {pet_name}\'s visit:',
+                                reply_markup=ReplyKeyboardMarkup([['Today']], one_time_keyboard=True))
     return GET_VISITED_DATE
  
 def case_exsits(case_id):
-    return case_id in patients
+    return redis_db.get('case_id')
 
 def unknown_command(update, context):
     update.message.reply_text('command not found! Please try agian.')
     return USER_MAIN_MANU
 
-def update_case_date(update, context):
+def update_case_date(update, context): 
     visit_date = update.message.text
+    if (visit_date=='Today'): #PICKEL SERIALIZE
+        visit_date = datetime.datetime.now()
+    else: #Print a nice calander on telegram inline keyboard
+        alaki = 2
     context.user_data['date'] = visit_date
-    update.message.reply_text('New Case had been created! Set Timer')
+    update.message.reply_text(f'Set Timer:',
+                                reply_markup=ReplyKeyboardMarkup([['30']], one_time_keyboard=True))
     return ADMIN_TIMER
 
 def set_reminder_timer(update, context):
@@ -104,30 +118,59 @@ def set_reminder_timer(update, context):
     pet_name = context.user_data['pet_name']
     case_id = context.user_data['case_id']
     latest_visit = context.user_data['date']
-    patients[case_id] = {}
-    patients[case_id] = {'id': None,
-                                    'first_name': None,
-                                    'is_bot': False,
-                                    'last_name': None,
-                                    'username': None, 
-                                    'language_code': None,
-                                    'next_alarm': next_alarm,
-                                    'latest_visit': latest_visit,
-                                    'visit_history': [latest_visit],
-                                    'pet_name': pet_name}
-    
-    del context.user_data['case_id']
-    
-    update.message.reply_text(f'I created a profile for {pet_name}, here is the details {patients[case_id]}')
+    patient = {
+                'guardian': list(),
+                'case': {
+                    'next_alarm': next_alarm,
+                    'medicine': list(),
+                    'latest_visit': latest_visit,
+                    'visit_history': [latest_visit],
+                    'pet_name': pet_name
+                }
+    }
 
-    admin_main_menu(update)
-    return ADMIN_MAIN_MANU
+    context.user_data.clear()
+    
+    redis_db.set(case_id, json.dumps(patient))
+    update.message.reply_text(f'I created a profile for {pet_name}, here is the details {patient}')
+
+    return admin_main_menu(update)
+
+def user_start(update, context):
+    usr = update.effective_user
+    name = usr['first_name']
+    update.message.reply_text(f'Hello {name}!')
+    return user_main_menu(update)
+
+def user_main_menu(update):
+    user_keyboard = [['case']]
+    update.message.reply_text(f'What do you want to do?',
+                                reply_markup=ReplyKeyboardMarkup(user_keyboard, one_time_keyboard=True))
+    return USER_MAIN_MANU
+
+def user_add_case(update, context):
+    update.message.reply_text("Please Enter the case ID")
+    return SET_CASE_ID 
+
+def user_get_case_id(update, context):
+    case_id = update.message.text
+    patient = redis_db.get(case_id)
+    if patient is not None:
+        patient = json.loads(patient)
+        tg_guardian = update.effective_user
+        if tg_guardian not in patient['guardian']:
+            patient['guardian'].append(tg_guardian.to_dict())
+            redis_db.set(case_id, json.dumps(patient))
+        else:
+            update.reply_text('You are already a registered guardian for this pet')
+    else:
+        update.message.reply_text('Are you sure about the case id? I didn\'t find anything!')
+
+    return user_main_menu(update)
 
 def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
-
-    #admin_handler = CommandHandler('admin_start', admin_start)
     
     admin_handler = ConversationHandler(
         entry_points = [CommandHandler('admin_start', admin_start)],
@@ -142,7 +185,7 @@ def main():
                             MessageHandler(Filters.regex('^all case info$'),
                             admin_all_case_info)
             ],
-            GET_CASE_ID:[MessageHandler(Filters.text,
+            SET_CASE_ID:[MessageHandler(Filters.text,
                                          get_case_id)
             ],
             SET_PET_NAME:[MessageHandler(Filters.text,
@@ -159,7 +202,24 @@ def main():
         fallbacks=[MessageHandler(Filters.regex('^done$'), unknown_command)]
     )
 
+    user_handler = ConversationHandler(
+        entry_points = [CommandHandler('user_start', user_start)],
+
+        states= {
+            USER_MAIN_MANU: [MessageHandler(Filters.regex('^case$'),
+                                            user_add_case)
+            ],
+
+            SET_CASE_ID:[MessageHandler(Filters.text,
+                                         user_get_case_id)
+            ],
+
+        },
+        fallbacks=[MessageHandler(Filters.regex('^done$'), unknown_command)]
+    )
+
     dp.add_handler(admin_handler)
+    dp.add_handler(user_handler)
     updater.start_polling()
 
     updater.idle()
